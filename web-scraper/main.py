ca429b17291b4abb330e74e24fbf81f2254d0f31
Manav Raja Vinotha <manav-the-dev@protonmail.com>
2022-06-17 13:33:15 -0500

    Some code for webscraper that scrapes attraction info for Saint Louis.

---
+++ web-scraper/main.py
@@ -0,0 +1,53 @@
+import random
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+
+domain = "https://www.tripadvisor.com"
+path_to_list_of_attractions = "/Attractions-g44881-Activities-a_allAttractions.true-Saint_Louis_Missouri.html"
+city = "Saint Louis"
+
+# Get links to all attractions on list of attractions page
+driver = webdriver.Firefox()
+driver.get(domain + path_to_list_of_attractions)
+driver_document = driver.page_source
+attraction_list_document = BeautifulSoup(driver_document, "html.parser")
+def get_attraction_list():
+    attraction_list_links_raw = attraction_list_document.find_all("div", {"class": "fVbwn cdAAV cagLQ eZTON"})
+    attraction_list_links = [attraction.a["href"] for attraction in attraction_list_links_raw] # Convert list of raw a tags to list of paths
+    return attraction_list_links
+attraction_list_links = get_attraction_list()
+driver.close()
+
+# Get rid of 29 elements of 30
+for i in range(29):
+    attraction_list_links.pop(0)
+
+# Visit each attraction link & scrape data
+for attraction in attraction_list_links:
+    driver = webdriver.Firefox()
+    #driver.get(domain + str(attraction))
+    driver.get("https://www.tripadvisor.com/Attraction_Review-g44881-d107811-Reviews-Missouri_Botanical_Garden-Saint_Louis_Missouri.html")
+    time.sleep(1)
+    driver_document = driver.page_source
+    attraction_document = BeautifulSoup(driver_document, "html.parser")
+
+    attraction_name = attraction_document.find("h1", {"class": "WlYyy cPsXC GeSzT"}).text
+    def get_attraction_address():
+        attraction_address_raw = attraction_document.find_all("span", {"class": "WlYyy cacGK Wb"})
+        for attraction_address in attraction_address_raw:
+            if city in attraction_address.text:
+                return attraction_address.text
+    attraction_address = get_attraction_address()
+    attraction_website_raw = attraction_document.find_all("span", {"class": "WlYyy cacGK Wb"})
+    def get_attraction_website():
+        for attraction_website in attraction_website_raw:
+            if "Visit website" in attraction_website.text:
+                return attraction_website.parent["href"]
+    attraction_website_link = get_attraction_website()
+    attraction_rating = attraction_document.find("div", {"class": "WlYyy cPsXC fksET cMKSg"}).text
+    
+    driver.close()
